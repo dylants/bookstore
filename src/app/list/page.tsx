8dee2021561185b594728cf33d2ae426c410cb05
Dylan Smith <dylantsmith@gmail.com>
2024-01-22 15:13:15 -0600

    force dynamic route
    This is necessary to make sure we load the data for this route on request, not during build time.

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,6 +1,12 @@
 import { getBooks } from "@/lib/actions";
 import Books from "@/components/Books";
 
+// NextJS will by default make this a static route, which will load
+// the data during build time. Instead we want this to be loaded
+// at request time (but on the server), so force a dynamic route.
+// https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
+export const dynamic = "force-dynamic";
+
 export default async function ListPage() {
   const books = await getBooks();
 
