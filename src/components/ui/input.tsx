e57c557408855dc864b648e2f960c3782f3f4f2a
Dylan Smith <dylantsmith@gmail.com>
2024-02-02 16:22:40 -0600

    chore: code coverage ignore
    - Ignore 3rd party files from code coverage. The prisma file was a direct copy from prisma setup, and the tailwind utils was added by Shadcn

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
-import { cn } from 'lib/utils';
+import { cn } from 'lib/tailwind-utils';
 
 const inputVariants = cva(
   'flex h-9 w-full rounded-md border border-customPalette-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50',
