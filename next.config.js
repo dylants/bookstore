7259791f64f4dbefa717452ae8de069c9ba75e3b
Dylan Smith <dylantsmith@gmail.com>
2024-02-14 15:08:02 -0600

    chore: parse BigInt for serialization in nextjs (#39)
    When the API responds with a value that includes a BigInt, it needs to parse it to JSON. This fails because JSON.stringify does not yet support BigInt. So provide an override as instructed by MDN. Include this in the next config so that it will be applied within the next framework (which includes the APIs).

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+// Some of our data types are BigInts, so override toJSON to handle
+// the failure to serialize: "Do not know how to serialize a BigInt"
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt#use_within_json
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
