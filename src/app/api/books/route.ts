1f714a5ea3ba8b91f6ce9f358414c161cba253e2
Dylan Smith <dylantsmith@gmail.com>
2024-02-16 12:42:46 -0600

    feat: create book API
    - Add API to create a book via a POST method
    - use `toZod` to help align the types

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,9 +1,12 @@
-import { getBooks } from '@/lib/actions/book';
+import { createBook, getBooks } from '@/lib/actions/book';
+import BookCreateInput from '@/types/BookCreateInput';
+import { Format, Genre } from '@prisma/client';
 import { NextRequest } from 'next/server';
+import { toZod } from 'tozod';
 import { z } from 'zod';
 import { fromZodError } from 'zod-validation-error';
 
-const schema = z.object({
+const getSchema = z.object({
   after: z.string().optional().nullable(),
   before: z.string().optional().nullable(),
   first: z.coerce.number().optional().nullable(),
@@ -15,7 +18,7 @@ export async function GET(request: NextRequest) {
   const params = Object.fromEntries(searchParams.entries());
   const { after, before, first, last } = params;
 
-  const validatedFields = schema.safeParse({
+  const validatedFields = getSchema.safeParse({
     after,
     before,
     first,
@@ -37,4 +40,46 @@ export async function GET(request: NextRequest) {
   });
 
   return Response.json(response);
+}
+
+const postSchema: toZod<BookCreateInput> = z.object({
+  authors: z.string(),
+  // toZod does not handle enums, so avoid this typecheck
+  format: z.nativeEnum(Format) as never,
+  // toZod does not handle enums, so avoid this typecheck
+  genre: z.nativeEnum(Genre) as never,
+  imageUrl: z.string().nullable(),
+  // toZod does not handle BigInts, so avoid this typecheck
+  // transform the BigInt/string as input to BigInt
+  isbn13: z
+    .any()
+    .transform((value) => {
+      try {
+        return BigInt(value);
+      } catch (error) {
+        return value;
+      }
+    })
+    .pipe(z.bigint()) as unknown as z.ZodBigInt,
+  publishedDate: z.coerce.date().nullable(),
+  publisher: z.string(),
+  title: z.string(),
+  vendorId: z.number(),
+});
+
+export async function POST(request: NextRequest) {
+  const book = await request.json();
+
+  const validatedFields = postSchema.safeParse(book);
+
+  if (!validatedFields.success) {
+    const message = fromZodError(validatedFields.error);
+    return new Response(null, {
+      status: 400,
+      statusText: message.toString(),
+    });
+  }
+
+  const createdBook = await createBook(book);
+  return Response.json(createdBook);
 }
