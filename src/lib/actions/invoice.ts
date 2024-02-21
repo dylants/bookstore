7e53a8ff6a9d75db93b1675797925d1d5acfdc82
Dylan Smith <dylantsmith@gmail.com>
2024-02-21 14:10:03 -0600

    feat: complete invoice
    When marking the invoice as complete, we iterate over the invoice items and increase the associated book's quantity with the quantity in the invoice item (we've received this amount of books). Once that's done, we mark the invoice as complete and include today's date.

--- src/lib/actions/invoice.ts
+++ src/lib/actions/invoice.ts
@@ -2,6 +2,7 @@ import logger from '@/lib/logger';
 import prisma from '@/lib/prisma';
 import InvoiceCreateInput from '@/types/InvoiceCreateInput';
 import InvoiceHydrated from '@/types/InvoiceHydrated';
+import { Invoice, InvoiceItem, Prisma } from '@prisma/client';
 
 export async function createInvoice(
   invoice: InvoiceCreateInput,
@@ -23,4 +24,50 @@ export async function createInvoice(
   logger.trace('created invoice in DB: %j', createdInvoice);
 
   return createdInvoice;
+}
+
+async function updateBookQuantity(
+  tx: Prisma.TransactionClient,
+  invoiceItem: InvoiceItem,
+): Promise<void> {
+  const { bookId, quantity } = invoiceItem;
+
+  const book = await tx.book.findUniqueOrThrow({
+    where: { id: bookId },
+  });
+
+  await tx.book.update({
+    data: { quantity: book.quantity + quantity },
+    where: { id: bookId },
+  });
+}
+
+export async function completeInvoice(
+  invoiceId: Invoice['id'],
+): Promise<InvoiceHydrated> {
+  const invoiceItems = await prisma.invoiceItem.findMany({
+    where: {
+      invoiceId: invoiceId,
+    },
+  });
+
+  return prisma.$transaction(async (tx) => {
+    logger.trace('updating books for %d invoice items...', invoiceItems.length);
+    await Promise.all(invoiceItems.map((item) => updateBookQuantity(tx, item)));
+
+    logger.trace('marking invoice complete, invoice id: %s', invoiceId);
+    const invoice = await tx.invoice.update({
+      data: {
+        dateReceived: new Date(),
+        isCompleted: true,
+      },
+      include: {
+        invoiceItems: true,
+        vendor: true,
+      },
+      where: { id: invoiceId },
+    });
+
+    return invoice;
+  });
 }
