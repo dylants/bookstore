8d8b1dd57159d046011ed0a38bd5aa0406db5b98
Dylan Smith <dylantsmith@gmail.com>
2024-02-23 17:50:12 -0600

    feat: new invoice item page (#88)
    - Add page to create a new invoice item. This page follows closely after the original add book page, which will most likely be removed.
    - Provide links to add a new invoice item and complete the invoice on the invoice page

--- src/types/BookFormInput.ts
+++ src/types/BookFormInput.ts
@@ -2,13 +2,14 @@ import BookCreateInput from '@/types/BookCreateInput';
 
 type BookFormInput = Omit<
   BookCreateInput,
-  'format' | 'genre' | 'isbn13' | 'priceInCents' | 'publishedDate'
+  'format' | 'genre' | 'isbn13' | 'priceInCents' | 'publishedDate' | 'quantity'
 > & {
   format: string;
   genre: string;
   isbn13: string;
   priceInCents: string;
   publishedDate: string;
+  quantity: string;
 };
 
 export default BookFormInput;
