4f1264cca52b46b5e1cd746c4907f668bf6a6805
Dylan Smith <dylantsmith@gmail.com>
2024-02-22 18:12:50 -0600

    feat: InvoiceItemsTable (#79)
    - Add table to display invoice items
    - make sure the invoice item action is server only, and make the invoice item fakes a bit more real

--- src/lib/fakes/invoice-item.ts
+++ src/lib/fakes/invoice-item.ts
@@ -6,7 +6,8 @@ import { InvoiceItem } from '@prisma/client';
 import _ from 'lodash';
 
 export function fakeInvoiceItem(): InvoiceItem {
-  const itemCostInCents = _.toNumber(faker.commerce.price({ max: 100 })) * 100;
+  const itemCostInCents =
+    _.toNumber(faker.commerce.price({ max: 100 })) * 100 + 99;
   const quantity =
     Math.random() > 0.3 ? 1 : faker.number.int({ max: 10, min: 1 });
   const totalCostInCents = itemCostInCents * quantity;
