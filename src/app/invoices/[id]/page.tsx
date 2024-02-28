de592ace69e84b33d24ff70089a42a46f566b300
Dylan Smith <dylantsmith@gmail.com>
2024-02-28 10:44:31 -0600

    feat: dialog for invoice item add (#94)
    - use a dialog to add an invoice item
    - refactor some of the code within the invoice item page into separate components. In doing so, we need to alert jest to not collect code coverage from anything within the app directory, other than the api files

--- src/app/invoices/[id]/page.tsx
+++ src/app/invoices/[id]/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import BookForm from '@/app/invoices/[id]/BookForm';
+import InvoiceDescription from '@/app/invoices/[id]/InvoiceDescription';
 import {
   Breadcrumbs,
   BreadcrumbsHome,
@@ -12,55 +14,16 @@ import { Button } from '@/components/ui/button';
 import { LoadingSpinner } from '@/components/ui/loading-spinner';
 import { completeInvoice, getInvoice } from '@/lib/actions/invoice';
 import { getInvoiceItems } from '@/lib/actions/invoice-item';
-import { discountPercentageToDisplayString } from '@/lib/money';
 import InvoiceHydrated from '@/types/InvoiceHydrated';
 import InvoiceItemHydrated from '@/types/InvoiceItemHydrated';
 import _ from 'lodash';
-import { usePathname, useRouter } from 'next/navigation';
 import { useCallback, useEffect, useState } from 'react';
-
-function InvoiceDescription({ invoice }: { invoice: InvoiceHydrated }) {
-  const { vendor } = invoice;
-  return (
-    <div className="flex w-full justify-between text-lg">
-      <div className="flex flex-col">
-        <div className="font-bold">{vendor.name}</div>
-        <div className="grid grid-cols-2 gap-0">
-          <div>
-            <div>Account number:</div>
-            <div>Discount:</div>
-          </div>
-          <div className="text-right">
-            <div>{vendor.accountNumber}</div>
-            <div>
-              {discountPercentageToDisplayString(vendor.discountPercentage)}
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="grid grid-cols-2 gap-1">
-        <div>
-          <div>Invoice Number:</div>
-          <div>Invoice Date:</div>
-          <div>Complete:</div>
-        </div>
-        <div className="text-right">
-          <div>{invoice.invoiceNumber}</div>
-          <div>{invoice.invoiceDate.toLocaleDateString()}</div>
-          <div>{invoice.isCompleted ? 'Yes' : 'No'}</div>
-        </div>
-      </div>
-    </div>
-  );
-}
 
 export default function InvoicePage({ params }: { params: { id: string } }) {
   const [invoice, setInvoice] = useState<InvoiceHydrated | null>();
   const [invoiceItems, setInvoiceItems] =
     useState<Array<InvoiceItemHydrated> | null>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const pathname = usePathname();
-  const router = useRouter();
 
   // TODO we should validate this input
   const invoiceId = _.toNumber(params.id);
@@ -118,30 +81,35 @@ export default function InvoicePage({ params }: { params: { id: string } }) {
         <BreadcrumbsText>{invoice.invoiceNumber}</BreadcrumbsText>
       </Breadcrumbs>
 
-      <div className="my-4">
-        <InvoiceDescription invoice={invoice} />
-      </div>
+      <InvoiceDescription invoice={invoice} />
 
-      <div className="flex flex-col gap-4 mt-8">
-        <div className="flex w-full justify-between gap-4">
+      {!invoice.isCompleted && (
+        <div className="flex justify-end mt-2">
           <Button
             variant="secondary"
-            disabled={invoice.isCompleted}
             onClick={async () => {
               await completeInvoice(invoiceId);
               loadInvoice();
             }}
           >
-            Publish
-          </Button>
-          <Button
-            variant="default"
-            disabled={invoice.isCompleted}
-            onClick={() => router.push(`${pathname}/add-invoice-item`)}
-          >
-            Add Item
+            Complete Invoice
           </Button>
         </div>
+      )}
+
+      <hr className="my-8 mx-8 bg-slate-300" />
+
+      {!invoice.isCompleted && (
+        <BookForm
+          invoice={invoice}
+          onCreateInvoiceItem={() => {
+            loadInvoiceItems();
+            // TODO add success
+          }}
+        />
+      )}
+
+      <div className="mt-4">
         <InvoiceItemsTable
           invoiceItems={invoiceItems || []}
           isLoading={!invoiceItems || isLoading}
