4d60cf9b35b1941ac1ba1654e1747f3024ff0cd7
Dylan Smith <dylantsmith@gmail.com>
2024-03-07 15:47:23 -0600

    feat: compute tax function (#116)
    - add a function to compute the tax of a product given the price
    - we will most likely want to store the tax percentage somewhere else, but for now just hard code in the file

--- src/lib/money.ts
+++ src/lib/money.ts
@@ -1,5 +1,8 @@
 import _ from 'lodash';
 
+// TODO should we store this somewhere else?
+const TAX = 0.0825;
+
 export function convertCentsToDollars(cents: string | number): number {
   return _.toNumber(cents) / 100;
 }
@@ -24,4 +27,12 @@ export function discountPercentageToDisplayString(
   discountPercentage: number | null,
 ): string {
   return `${_.round((discountPercentage ?? 0) * 100, 2)}%`;
+}
+
+export function computeTax(priceInCents: number): number {
+  if (priceInCents < 0) {
+    return 0;
+  }
+
+  return _.round(priceInCents * TAX);
 }
