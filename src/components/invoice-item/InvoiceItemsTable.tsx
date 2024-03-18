c578101368daf2ddc21e8ddd5cc7f69f3c4ca0e2
Dylan Smith <dylantsmith@gmail.com>
2024-03-18 16:47:55 -0500

    feat: Show discount % in InvoiceItemsTable (#124)
    - add the discount percentage, along with the full book price to the invoice items table. If the item is not a book, we must show "unknown" as the discount percentage b/c we can't get the item's price.
    - add TODO to make the discount percentage dynamic on input

--- src/components/invoice-item/InvoiceItemsTable.tsx
+++ src/components/invoice-item/InvoiceItemsTable.tsx
@@ -1,6 +1,10 @@
 import Dollars from '@/components/Dollars';
 import DataTable from '@/components/table/DataTable';
 import SortableHeader from '@/components/table/SortableHeader';
+import {
+  determineDiscountPercentage,
+  discountPercentageToDisplayString,
+} from '@/lib/money';
 import BookHydrated from '@/types/BookHydrated';
 import InvoiceItemHydrated from '@/types/InvoiceItemHydrated';
 import { ColumnDef } from '@tanstack/react-table';
@@ -42,13 +46,46 @@ const columns: ColumnDef<InvoiceItemHydrated>[] = [
     id: 'title',
   },
   {
+    accessorFn: (invoiceItem) => invoiceItem.book,
+    cell: (props) => {
+      const book = props.getValue() as BookHydrated | null;
+
+      return (
+        <div className="text-right">
+          <Dollars amountInCents={book?.priceInCents ?? 0} />
+        </div>
+      );
+    },
+    header: ({ column }) => <SortableHeader column={column} text="Price" />,
+    id: 'price',
+  },
+  {
+    accessorFn: (invoiceItem) => invoiceItem,
+    cell: (props) => {
+      const invoiceItem = props.getValue() as InvoiceItemHydrated;
+
+      const discountPercentageToDisplay = invoiceItem.book
+        ? discountPercentageToDisplayString(
+            determineDiscountPercentage({
+              discountedPriceInCents: invoiceItem.itemCostInCents,
+              fullPriceInCents: invoiceItem.book.priceInCents,
+            }),
+          )
+        : 'Unknown';
+
+      return <div className="text-right">{discountPercentageToDisplay}</div>;
+    },
+    header: ({ column }) => <SortableHeader column={column} text="Discount" />,
+    id: 'discountPercentage',
+  },
+  {
     accessorKey: 'itemCostInCents',
     cell: (props) => (
       <div className="text-right">
         <Dollars amountInCents={props.getValue() as number} />
       </div>
     ),
-    header: ({ column }) => <SortableHeader column={column} text="Item Cost" />,
+    header: ({ column }) => <SortableHeader column={column} text="Cost" />,
   },
   {
     accessorKey: 'quantity',
@@ -57,7 +94,7 @@ const columns: ColumnDef<InvoiceItemHydrated>[] = [
         <>{props.getValue()}</>
       </div>
     ),
-    header: ({ column }) => <SortableHeader column={column} text="Quantity" />,
+    header: ({ column }) => <SortableHeader column={column} text="#" />,
   },
   {
     accessorKey: 'totalCostInCents',
@@ -66,9 +103,7 @@ const columns: ColumnDef<InvoiceItemHydrated>[] = [
         <Dollars amountInCents={props.getValue() as number} />
       </div>
     ),
-    header: ({ column }) => (
-      <SortableHeader column={column} text="Total Cost" />
-    ),
+    header: ({ column }) => <SortableHeader column={column} text="Total" />,
   },
 ];
 
