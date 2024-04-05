8309f84691a8c642c2b6b86177cc7ddf6dd3e408
Dylan Smith <dylantsmith@gmail.com>
2024-04-05 17:06:57 -0500

    feat: safeActionWrapper (#166)
    - Extract out the shared code which wrapped the function call in the safe functions.
    - The existing tests should cover the refactor

--- src/lib/actions/order-safe.ts
+++ src/lib/actions/order-safe.ts
@@ -2,35 +2,12 @@
 
 import { deleteOrder } from '@/lib/actions/order';
 import BadRequestError from '@/lib/errors/BadRequestError';
+import { safeActionWrapper } from '@/lib/safe-action-wrapper';
 import { HttpResponse } from '@/types/HttpResponse';
 import { Order } from '@prisma/client';
 
 export async function deleteOrderSafe(
   orderUID: Order['orderUID'],
-): Promise<HttpResponse<null, BadRequestError>> {
-  try {
-    await deleteOrder(orderUID);
-
-    return {
-      data: null,
-      status: 200,
-    };
-  } catch (err: unknown) {
-    if (err instanceof BadRequestError) {
-      return {
-        data: null,
-        error: {
-          ...err,
-          message: err.message,
-          name: err.name,
-        },
-        status: 400,
-      };
-    }
-
-    return {
-      data: null,
-      status: 500,
-    };
-  }
+): Promise<HttpResponse<void | null, BadRequestError>> {
+  return safeActionWrapper(deleteOrder, orderUID);
 }
