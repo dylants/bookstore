426c8c2001e3099876b45b34c9a1508789b89846
Dylan Smith <dylantsmith@gmail.com>
2024-04-17 12:54:18 -0500

    feat: SearchCommandContainer (#171)
    - add the search command container which wraps the search command component providing business logic. Hold off on navigation at the moment until we get a book details page.
    - replace the search page with the search command container/component in the nav
    - using debounce within useCallback can be tricky, so pull in useDebounceCallback

---
+++ src/app/search/SearchCommandContainer.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import SearchCommand, {
+  SearchCommandResult,
+} from '@/components/search/SearchCommand';
+import { findBooksBySearchString } from '@/lib/actions/book';
+import BookHydrated from '@/types/BookHydrated';
+import { useDebounceCallback } from '@react-hook/debounce';
+import { useCallback, useState } from 'react';
+
+function buildSearchResult(book: BookHydrated): SearchCommandResult {
+  const authors = book.authors.map((author) => author.name).join(' ');
+  const text = `${book.title} by ${authors}`;
+
+  return {
+    id: book.id.toString(),
+    imageUrl: book.imageUrl,
+    text,
+  };
+}
+
+export default function SearchCommandContainer() {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [results, setResults] = useState<Array<BookHydrated> | null>(null);
+  const [isSearching, setIsSearching] = useState<boolean>(false);
+
+  const onClose = useCallback(() => {
+    setResults(null);
+    setSearchValue('');
+    setIsSearching(false);
+  }, []);
+
+  const search = useDebounceCallback(async (searchString) => {
+    // short circuit to skip the search
+    if (!searchString) {
+      setResults(null);
+      return;
+    }
+
+    setIsSearching(true);
+    const books = await findBooksBySearchString(searchString);
+    setResults(books);
+    setIsSearching(false);
+  }, 500);
+
+  const onSearchValueChange = useCallback(
+    (updatedSearchValue: string) => {
+      setSearchValue(updatedSearchValue);
+      search(updatedSearchValue);
+    },
+    [search],
+  );
+
+  const onSelect = useCallback(
+    (result: SearchCommandResult) => {
+      // TODO navigation
+      console.log(result);
+      onClose();
+    },
+    [onClose],
+  );
+
+  return (
+    <SearchCommand
+      isSearching={isSearching}
+      onClose={onClose}
+      onSearchValueChange={onSearchValueChange}
+      onSelect={onSelect}
+      results={results ? results.map(buildSearchResult) : null}
+      searchValue={searchValue}
+    />
+  );
+}
